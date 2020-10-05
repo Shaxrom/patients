1bc6fec4b63639cbd589490b4793619f35ed19f9
ShukurovSh <59442362+Shaxrom@users.noreply.github.com>
2020-10-05 14:08:53 +0500

    first commit

---
+++ src/test/java/globalSolution/interwiev/patients/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package globalSolution.interwiev.patients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
